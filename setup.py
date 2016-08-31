3848971bafae4806fb9a36298eabc465a6fdaf76
Thomas Dane <thomasgdane@gmail.com>
2016-08-31 16:45:22 +0100

    new setup.py using setuptools

--- setup.py
+++ setup.py
@@ -1,24 +1,50 @@
-
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
 from codecs import open
 from os import path
 
-from distutils.core import setup
+here = path.abspath(path.dirname(__file__))
 
-packages=[
-	'',
-	'pygix',
-]
-package_dir = {'':'lib'}
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+packages=find_packages(exclude=['docs', 'tests'])
+
+# double check classifiers on https://pypi.python.org/pypi?%3Aaction=list_classifiers
+classifiers = ["Development Status :: 4 - Beta",
+               "Intended Audience :: Developers",
+               "Programming Language :: Python :: 2",
+               "Programming Language :: Python :: 2.7",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: GNU General Public License (GPL)",
+               "License :: OSI Approved :: MIT License",
+               "Topic :: Software Development :: Libraries :: Python Modules",
+               "Operating System :: Microsoft :: Windows",
+               "Operating System :: Unix",
+               "Operating System :: MacOS :: MacOS X",
+               "Topic :: Scientific/Engineering :: Physics"
+               ]
+
+install_requires = ["numpy", "h5py", "fabio", "matplotlib", "scipy", "pyFAI"]
+setup_requires = ["numpy", "cython", "pyFAI"]
 
 setup(
-	name          =   "pygix",
-	version       =   "0.1.1",
-	description   =   "Grazing-incidence X-ray scattering package",
-	author        =   "Thomas Dane",
-	author_email  =   "dane@esrf.fr",
-	packages      =   packages,
-	package_dir   =   package_dir,
-)
+    name='pygix',
+    version='0.1.2',    
+    description='Reduction of 2D Grazing-incidence and fibre diffraction data',
+    long_description=long_description,
+    url='https://github.com/tgdane/pygix',
+    author='Thomas Dane',
+    author_email='thomasgdane@gmail.com',
+    licence="GPL",
+    classifiers=classifiers,
+    packages=packages,
+    install_requires=install_requires,
+    setup_requires=setup_requires
+    )
+
+
+
+
+
