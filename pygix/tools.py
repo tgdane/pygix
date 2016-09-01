a1abd7992f90a1f8e1d7b8cd3188d82ca619feb9
Thomas Dane <thomasgdane@gmail.com>
2016-09-01 13:21:20 +0100

    add module docstring

--- pygix/tools.py
+++ pygix/tools.py
@@ -1,5 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+""" A collection of useful tools for GIXS and fibre diffraction.
+
+There are two main tools:
+
+1. Four quadrant averaging:
+    Fibre diffraction patterns are related through four quadrant symmetry
+    (assuming ideal fibre texture). This means that the data in each of the
+    four quadrants are equivalent.[1] This allows us to cut out each quadrant,
+    centre, rotate and overlay, then average the sum of all four. This has two
+    advantages. Firstly, statistics are improved. Secondly, regions of missing
+    data (detector module gaps, asymmetric splitting due to large incident
+    angle) are filled from other quadrants - prettier images!
+
+    Example usage:
+        averaged, x, y = quadrant_average(data, x, y)
+
+    N.B. The span of the image will change and will be determined by
+    -absmax(q)) to +absmax(q) such that new x and y scales are returned.
+
+2. Integration ROIs:
+    Arrays defining the regions-of-interest (ROIs) for 1D data reduction methods
+    can be calculated. These are useful for overlaying on diffraction patterns
+    in figures to show the regions of data that have been integrated. Four
+    methods exist for each of the four 1D reduction methods:
+        sector_roi
+        chi_roi
+        op_box_roi
+        ip_box_roi
+
+    These functions take the same parameters as the integration methods.
+
+    N.B. These are all calculated in the output coordinate space, so must be
+    overlayed onto transformed images, NOT raw images.
+
+References:
+    [1] Stribeck and Nöchel, J. Appl. Crystallogr., (2009), 42, 295.
+"""
+
 import numpy as np
 
 
@@ -9,7 +48,8 @@ def quadrant_average(data, x=None, y=None, dummy=0):
     patterns. If only the data array is given, the function 
     assumes the center of reciprocal space is the central pixel
     and the returned averaged array will have the same shape as 
-    the input data. 
+    the input data. In general this should be avoided, it is safest if
+    x and y scales are provided!
 
     If x and y scaling are given, it deduces the
     center from these arrays. One quadrant will have the size of 
@@ -89,7 +129,7 @@ def sector_roi(chi_pos=None, chi_width=None, radial_range=None):
     if (len([x for x in [chi_pos, chi_width, radial_range] if x is not None]) is 0) \
             or (radial_range is None):
         raise RuntimeError('Integration over whole image, no ROI to display.')
-    return calc_sector(radial_range, chi_pos, chi_width)
+    return _calc_sector(radial_range, chi_pos, chi_width)
 
 
 def chi_roi(radial_pos, radial_width, chi_range=None):
@@ -112,7 +152,7 @@ def chi_roi(radial_pos, radial_width, chi_range=None):
 
     radial_min = radial_pos - radial_width / 2.0
     radial_max = radial_pos + radial_width / 2.0
-    return calc_sector((radial_min, radial_max), chi_pos, chi_width)
+    return _calc_sector((radial_min, radial_max), chi_pos, chi_width)
 
 
 def op_box_roi(ip_pos, ip_width, op_range):
@@ -128,7 +168,7 @@ def op_box_roi(ip_pos, ip_width, op_range):
     """
     ip_min = ip_pos - ip_width / 2.0
     ip_max = ip_pos + ip_width / 2.0
-    return calc_box((ip_min, ip_max), op_range)
+    return _calc_box((ip_min, ip_max), op_range)
 
 
 def ip_box_roi(op_pos, op_width, ip_range):
@@ -144,10 +184,10 @@ def ip_box_roi(op_pos, op_width, ip_range):
     """
     op_min = op_pos - op_width / 2.0
     op_max = op_pos + op_width / 2.0
-    return calc_box(ip_range, (op_min, op_max))
+    return _calc_box(ip_range, (op_min, op_max))
 
 
-def calc_sector(radial_range, chi_pos, chi_width):
+def _calc_sector(radial_range, chi_pos, chi_width):
     """Main function for calculating sector region of interest.
     Called by sector_roi and chi_roi.
 
@@ -160,7 +200,8 @@ def calc_sector(radial_range, chi_pos, chi_width):
         qr, qz (tuple of ndarrays): arrays defining the region of interest.
     """
     if len([x for x in [chi_pos, chi_width] if x is not None]) is 1:
-        raise RuntimeError('both chi_pos and chi_width must be supplied or neither')
+        raise RuntimeError('both chi_pos and chi_width must be supplied or '
+                           'neither')
 
     if (chi_pos is None) and (chi_width is None):
         chi_min = 0
@@ -194,7 +235,7 @@ def calc_sector(radial_range, chi_pos, chi_width):
     return qr, qz
 
 
-def calc_box(ip_range, op_range):
+def _calc_box(ip_range, op_range):
     """Main function for calculating box regions of interest.
     Called by op_box_roi and ip_box_roi.
 
