50c89e69475d71a52dc81c99ca68cfeadc064458
Thomas Dane <thomasgdane@gmail.com>
2016-09-01 10:02:04 +0100

    update __init__.py

--- pygix/__init__.py
+++ pygix/__init__.py
@@ -1,11 +1,17 @@
-version = "0.1.0"
-date = "2014-11"
-import sys, logging
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+
 logging.basicConfig()
 
 if sys.version_info < (2, 6):
     logger = logging.getLogger("pygix.__init__")
     logger.error("pygix requires a python version >= 2.6")
-    raise RuntimeError("pygix requires a python version >= 2.6, now we are running: %s" % sys.version)
-
-from transform import Transform
+    raise RuntimeError(
+        "pygix requires a python version >= 2.6, now we are running: %s" %
+        sys.version
+        )
+else:
+    from transform import Transform
