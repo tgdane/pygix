ea494f5e1c3bc204f694b762fa8d9016cdea1efd
Thomas Dane <thomasgdane@gmail.com>
2016-09-08 09:13:46 +0100

    add saving for integration ROIs

--- pygix/tools.py
+++ pygix/tools.py
@@ -40,6 +40,7 @@ References:
 """
 
 import numpy as np
+from . import io
 
 
 def quadrant_average(data, x=None, y=None, dummy=0):
@@ -117,35 +118,47 @@ def quadrant_average(data, x=None, y=None, dummy=0):
         return out_full
 
 
-def sector_roi(chi_pos=None, chi_width=None, radial_range=None):
+def sector_roi(chi_pos=None, chi_width=None, radial_range=None, filename=None):
     """Generate array defining region of interest for sector integration.
 
     Args:
         chi_pos (float): chi angle (deg) defining the centre of the sector.
         chi_width (float): width (deg) of sector.
         radial_range (tuple): integration range (min, max).
+        filename (string): filename to save the arrays.
 
     Returns:
         qr, qz (tuple of ndarrays): arrays defining the region of interest.
     """
+    param = locals()
+    param.pop('filename')
+
     if (len([x for x in [chi_pos, chi_width, radial_range] if
              x is not None]) is 0) \
             or (radial_range is None):
         raise RuntimeError('Integration over whole image, no ROI to display.')
-    return _calc_sector(radial_range, chi_pos, chi_width)
+    roi_x, roi_y = _calc_sector(radial_range, chi_pos, chi_width)
+
+    if filename:
+        io.save_roi(roi_x, roi_y, filename, **param)
+    return roi_x, roi_y
 
 
-def chi_roi(radial_pos, radial_width, chi_range=None):
+def chi_roi(radial_pos, radial_width, chi_range=None, filename=None):
     """Generate array defining region of interest for chi integration.
 
     Args:
         radial_pos (float): position defining the radius of the sector.
         radial_width (float): width (q or 2th) of sector.
         chi_range (tuple): azimuthal range (min, max).
+        filename (string): filename to save the arrays.
 
     Returns:
         qr, qz (tuple of ndarrays): arrays defining the region of interest.
     """
+    param = locals()
+    param.pop('filename')
+
     if (chi_range is None) or (chi_range[0] + chi_range[1] is 360):
         chi_width = None
         chi_pos = None
@@ -155,39 +168,59 @@ def chi_roi(radial_pos, radial_width, chi_range=None):
 
     radial_min = radial_pos - radial_width / 2.0
     radial_max = radial_pos + radial_width / 2.0
-    return _calc_sector((radial_min, radial_max), chi_pos, chi_width)
+    roi_x, roi_y = _calc_sector((radial_min, radial_max), chi_pos, chi_width)
+
+    if filename:
+        io.save_roi(roi_x, roi_y, filename, **param)
+    return roi_x, roi_y
 
 
-def op_box_roi(ip_pos, ip_width, op_range):
+def op_box_roi(ip_pos, ip_width, op_range, filename=None):
     """Generate array defining region of interest for out-of-plane box integration.
 
     Args:
         ip_pos (float): in-plane centre of integration box.
         ip_width (float): in-plane width of integration box.
         op_range (tuple): out-of-plane range (min, max).
+        filename (string): filename to save the arrays.
 
     Returns:
         qr, qz (tuple of ndarrays): arrays defining the region of interest.
     """
+    param = locals()
+    param.pop('filename')
+
     ip_min = ip_pos - ip_width / 2.0
     ip_max = ip_pos + ip_width / 2.0
-    return _calc_box((ip_min, ip_max), op_range)
+    roi_x, roi_y = _calc_box((ip_min, ip_max), op_range)
+
+    if filename:
+        io.save_roi(roi_x, roi_y, filename, **param)
+    return roi_x, roi_y
 
 
-def ip_box_roi(op_pos, op_width, ip_range):
+def ip_box_roi(op_pos, op_width, ip_range, filename=None):
     """Generate array defining region of interest for in-plane box integration.
 
     Args:
         op_pos (float): out-of-plane centre of integration box.
         op_width (float): out-of-plane width of integration box.
         ip_range (tuple): in-plane range (min, max).
+        filename (string): filename to save the arrays.
 
     Returns:
         qr, qz (tuple of ndarrays): arrays defining the region of interest.
     """
+    param = locals()
+    param.pop('filename')
+
     op_min = op_pos - op_width / 2.0
     op_max = op_pos + op_width / 2.0
-    return _calc_box(ip_range, (op_min, op_max))
+    roi_x, roi_y = _calc_box(ip_range, (op_min, op_max))
+
+    if filename:
+        io.save_roi(roi_x, roi_y, filename, **param)
+    return roi_x, roi_y
 
 
 def _calc_sector(radial_range, chi_pos, chi_width):
@@ -217,7 +250,7 @@ def _calc_sector(radial_range, chi_pos, chi_width):
 
     chi = np.radians(np.linspace(chi_min, chi_max, npts))
 
-    # lower
+    # lower part of arc
     if radial_range[0] is 0:
         lo_qr = np.array(0)
         lo_qz = np.array(0)
@@ -225,7 +258,7 @@ def _calc_sector(radial_range, chi_pos, chi_width):
         lo_qr = radial_range[0] * np.cos(chi)
         lo_qz = radial_range[0] * np.sin(chi)
 
-    # upper
+    # upper part of arc
     hi_qr = (radial_range[1] * np.cos(chi))[::-1]
     hi_qz = (radial_range[1] * np.sin(chi))[::-1]
 
