75a412e456638cb63067c5cfe5300c2b5e7102e7
Thomas Dane <thomasgdane@gmail.com>
2016-10-03 11:06:06 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,50 +1,226 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
+import sys
+import os
+import platform
 
-here = path.abspath(path.dirname(__file__))
+from numpy.distutils.misc_util import Configuration
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+try:
+    from setuptools import setup
+    from setuptools.command.build_py import build_py as _build_py
+    from setuptools.command.build_ext import build_ext
+    from setuptools.command.sdist import sdist
+except ImportError:
+    from numpy.distutils.core import setup
+    from distutils.command.build_py import build_py as _build_py
+    from distutils.command.build_ext import build_ext
+    from distutils.command.sdist import sdist
 
-packages=find_packages(exclude=['docs', 'tests'])
+PROJECT = "pygix"
+cmdclass = {}
 
-# double check classifiers on https://pypi.python.org/pypi?%3Aaction=list_classifiers
-classifiers = ["Development Status :: 4 - Beta",
+
+# Check if action requires build/install
+DRY_RUN = len(sys.argv) == 1 or (len(sys.argv) >= 2 and (
+    '--help' in sys.argv[1:] or
+    sys.argv[1] in ('--help-commands', 'egg_info', '--version',
+                    'clean', '--name')))
+
+
+def get_version():
+    import version
+    return version.strictversion
+
+
+def get_readme():
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(dirname, "README.md"), "r") as fp:
+        long_description = fp.read()
+    return long_description
+
+
+classifiers = ["Development Status :: 2 - Pre-alpha",
                "Intended Audience :: Developers",
+               "Intended Audience :: Education",
+               "Intended Audience :: Science/Research",
+               "Natural Language :: English",
+               "Programming Language :: Cython",
                "Programming Language :: Python :: 2",
                "Programming Language :: Python :: 2.7",
+               "Programming Language :: Python :: Implementation :: CPython",
                "Environment :: Console",
-               "Intended Audience :: Science/Research",
-               "License :: OSI Approved :: GNU General Public License (GPL)",
+               "Environment :: MacOS X",
+               "Environment :: Win32 (MS Windows)",
+               "License :: OSI Approved :: GPLv3+",
                "License :: OSI Approved :: MIT License",
-               "Topic :: Software Development :: Libraries :: Python Modules",
                "Operating System :: Microsoft :: Windows",
                "Operating System :: Unix",
+               "Operating System :: POSIX",
                "Operating System :: MacOS :: MacOS X",
+               "Topic :: Documentation :: Sphinx",
                "Topic :: Scientific/Engineering :: Physics"
+               "Topic :: Software Development :: Libraries :: Python Modules",
                ]
 
-install_requires = ["numpy", "h5py", "fabio", "matplotlib", "scipy", "pyFAI"]
-setup_requires = ["numpy", "cython", "pyFAI"]
 
-setup(
-    name='pygix',
-    version='0.1.3',
-    description='Reduction of 2D Grazing-incidence and fibre diffraction data',
-    long_description=long_description,
-    url='https://github.com/tgdane/pygix',
-    author='Thomas Dane',
-    author_email='thomasgdane@gmail.com',
-    licence="GPL",
+# ########## #
+# version.py #
+# ########## #
+
+
+class build_py(_build_py):
+    """
+    Enhanced build_py which copies version.py to <PROJECT>._version.py
+    """
+    def find_package_modules(self, package, package_dir):
+        modules = _build_py.find_package_modules(self, package, package_dir)
+        if package == PROJECT:
+            modules.append((PROJECT, '_version', 'version.py'))
+        return modules
+
+
+cmdclass['build_py'] = build_py
+
+
+# ################### #
+# build_doc commands  #
+# ################### #
+
+try:
+    import sphinx
+    import sphinx.util.console
+    sphinx.util.console.color_terminal = lambda: False
+    from sphinx.setup_command import BuildDoc
+except ImportError:
+    sphinx = None
+else:
+    # i.e. if sphinx:
+    class build_doc(BuildDoc):
+
+        def run(self):
+            # make sure the python path is pointing to the newly built
+            # code so that the documentation is built on this and not a
+            # previously installed version
+
+            build = self.get_finalized_command('build')
+            sys.path.insert(0, os.path.abspath(build.build_lib))
+
+#             # Copy .ui files to the path:
+#             dst = os.path.join(
+#                 os.path.abspath(build.build_lib), "silx", "gui")
+#             if not os.path.isdir(dst):
+#                 os.makedirs(dst)
+#             for i in os.listdir("gui"):
+#                 if i.endswith(".ui"):
+#                     src = os.path.join("gui", i)
+#                     idst = os.path.join(dst, i)
+#                     if not os.path.exists(idst):
+#                         shutil.copy(src, idst)
+
+            # Build the Users Guide in HTML and TeX format
+            for builder in ('html', 'latex'):
+                self.builder = builder
+                self.builder_target_dir = os.path.join(self.build_dir, builder)
+                self.mkpath(self.builder_target_dir)
+                BuildDoc.run(self)
+            sys.path.pop(0)
+    cmdclass['build_doc'] = build_doc
+
+
+# ############################# #
+# numpy.distutils Configuration #
+# ############################# #
+
+
+def configuration(parent_package='', top_path=None):
+    """Recursive construction of package info to be used in setup().
+    See http://docs.scipy.org/doc/numpy/reference/distutils.html#numpy.distutils.misc_util.Configuration
+    """  # noqa
+    config = Configuration(None, parent_package, top_path)
+    config.set_options(
+        ignore_setup_xxx_py=True,
+        assume_default_configuration=True,
+        delegate_options_to_subpackages=True,
+        quiet=True)
+    config.add_subpackage(PROJECT)
+    return config
+
+
+config = configuration()
+
+
+################################################################################
+# Debian source tree
+################################################################################
+
+
+class sdist_debian(sdist):
+    """
+    Tailor made sdist for debian
+    * remove auto-generated doc
+    * remove cython generated .c files
+    """
+    def prune_file_list(self):
+        sdist.prune_file_list(self)
+        to_remove = ["doc/build", "doc/pdf", "doc/html", "pylint", "epydoc"]
+        print("Removing files for debian")
+        for rm in to_remove:
+            self.filelist.exclude_pattern(pattern="*", anchor=False, prefix=rm)
+
+        # this is for Cython files specifically: remove C & html files
+        search_root = os.path.dirname(os.path.abspath(__file__))
+        for root, _, files in os.walk(search_root):
+            for afile in files:
+                if os.path.splitext(afile)[1].lower() == ".pyx":
+                    base_file = os.path.join(root, afile)[len(search_root) + 1:-4]
+                    self.filelist.exclude_pattern(pattern=base_file + ".c")
+                    self.filelist.exclude_pattern(pattern=base_file + ".cpp")
+                    self.filelist.exclude_pattern(pattern=base_file + ".html")
+
+    def make_distribution(self):
+        self.prune_file_list()
+        sdist.make_distribution(self)
+        dest = self.archive_files[0]
+        dirname, basename = os.path.split(dest)
+        base, ext = os.path.splitext(basename)
+        while ext in [".zip", ".tar", ".bz2", ".gz", ".Z", ".lz", ".orig"]:
+            base, ext = os.path.splitext(base)
+        if ext:
+            dest = "".join((base, ext))
+        else:
+            dest = base
+        sp = dest.split("-")
+        base = sp[:-1]
+        nr = sp[-1]
+        debian_arch = os.path.join(dirname, "-".join(base) + "_" + nr + ".orig.tar.gz")
+        os.rename(self.archive_files[0], debian_arch)
+        self.archive_files = [debian_arch]
+        print("Building debian .orig.tar.gz in %s" % self.archive_files[0])
+
+cmdclass['debian_src'] = sdist_debian
+
+
+# ##### #
+# setup #
+# ##### #
+
+setup_kwargs = config.todict()
+
+
+install_requires = ["numpy", "pyFAI"]
+setup_requires = ["numpy"]
+
+setup_kwargs.update(
+    name=PROJECT,
+    version=get_version(),
+    url="https://github.com/tgdane/pygix",
+    author="Thomas Dane",
+    author_email="thomasgdane@gmail.com",
     classifiers=classifiers,
-    packages=packages,
-    # install_requires=install_requires,
-    # setup_requires=setup_requires
+    description="Software library for grazing-incidence/fibre X-Ray data analysis",
+    long_description=get_readme(),
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    cmdclass=cmdclass,
     )
 
-
-
-
-
+setup(**setup_kwargs)
